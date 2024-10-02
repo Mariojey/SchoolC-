087619c531d860bf219c487609614d31eb8810f4
Mariusz Jacek <marios.yaceg@gmail.com>
2024-10-02 20:42:07 +0200

    Alg1

--- alg1.cpp
+++ alg1.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <cstdlib>
 
 using namespace std;
 
@@ -8,6 +7,7 @@ int main(int argc, char* argv[]){
     int sum_np = 0;
     for(int i = 1; i < argc; i++){
         int l = atoi(argv[i]);
+        cout<<l<<endl;
         if(l % 2 == 0){
             sum_p += l;
         }else if(l % 2 != 0){
@@ -15,5 +15,5 @@ int main(int argc, char* argv[]){
         }
     }
     cout<<"Suma liczb parzystych = "<<sum_p<<endl;
-    cout<<"Suma liczb parzystych = "<<sum_np<<endl;
+    cout<<"Suma liczb nieparzystych = "<<sum_np<<endl;
 }
---
+++ spike.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    char c = '7';
+    int d;
+    d = c - '0';
+    cout<<d;
+    return 0;
+}
