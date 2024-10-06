7d4710987e4d6cb392293d374b42f68fcd9d9e6d
Mariusz Jacek <marios.yaceg@gmail.com>
2024-10-06 11:25:05 +0200

    convert function

--- alg1.cpp
+++ alg1.cpp
@@ -2,11 +2,30 @@
 
 using namespace std;
 
+int convert(const char* text){
+    int value = 0;
+    int sign = 1;
+    int i = 0;
+
+    if (text[0] == '-')
+    {
+        sign = -1;
+        i = 1;
+    }
+
+    for(; text[i] != '\0'; ++i){
+        value = value * 10 + (text[i] - '0');
+    }
+
+    return sign * value;
+    
+}
+
 int main(int argc, char* argv[]){
     int sum_p = 0;
     int sum_np = 0;
     for(int i = 1; i < argc; i++){
-        int l = atoi(argv[i]);
+        int l = convert(argv[i]);
         cout<<l<<endl;
         if(l % 2 == 0){
             sum_p += l;
