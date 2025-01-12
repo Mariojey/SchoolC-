83383fcc3f6f9ddd99973dfaef096b9d1a8c908a
Mariusz Jacek <marios.yaceg@gmail.com>
2025-01-12 12:21:10 +0100

    Insertion and selection sort

--- Revision/sortowania.cpp
+++ Revision/sortowania.cpp
@@ -15,6 +15,32 @@ void bubbleSort(int arr[], int n){
   }
 }
 
+void selectionSort(int arr[], int n){
+  for(int i=0;i<n-1;i++){
+    int min = i;
+    for(int j=i+1;j<n;j++){
+      if(arr[j]<arr[min]){
+        min = j;
+      }
+    }
+    int temp = arr[i];
+    arr[i] = arr[min];
+    arr[min] = temp;
+  }
+}
+
+void insertionSort(int arr[], int n){
+  for(int i=1;i<n;i++){
+    int temp = arr[i];
+    int j = i-1;
+    while(j>=0 && arr[j]>temp){
+      arr[j+1] = arr[j];
+      j = j-1;
+    }
+    arr[j+1] = temp;
+  }
+}
+
 int main(int argc, char * argv[]) {
 
   string sortType = argv[1];
